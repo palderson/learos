94dae2eadb1ab6a8b5917081b35fddada6b009b2
Mirko J <mirko114@gmail.com>
2013-07-16 00:52:20 +0200

    db/schema.rb updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130715083023) do
+ActiveRecord::Schema.define(:version => 20130715100256) do
+
+  create_table "goals", :force => true do |t|
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "goals", ["project_id"], :name => "index_goals_on_project_id"
 
   create_table "overviews", :force => true do |t|
     t.text     "description"
