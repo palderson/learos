e2d4ff4408dff8953f9682207303f98c6cb5cb48
Mirko J <mirko114@gmail.com>
2013-07-15 11:30:53 +0200

    db/schema.rb updated

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(:version => 20130715083023) do
 
   create_table "overviews", :force => true do |t|
-    t.string   "description"
+    t.text     "description"
     t.integer  "project_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
