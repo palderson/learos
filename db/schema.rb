3b67b835e79832aca19fd225ca3becfb0089d214
Mirko J <mirko114@gmail.com>
2013-07-26 10:15:08 +0200

    db/schema.rb updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130721220840) do
+ActiveRecord::Schema.define(:version => 20130726071638) do
 
   create_table "billings", :force => true do |t|
     t.text     "payment_processing_method"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(:version => 20130721220840) do
   end
 
   add_index "client_trainings", ["project_id"], :name => "index_client_trainings_on_project_id"
+
+  create_table "collaborations", :force => true do |t|
+    t.integer  "project_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "collaborations", ["project_id"], :name => "index_collaborations_on_project_id"
+  add_index "collaborations", ["user_id"], :name => "index_collaborations_on_user_id"
 
   create_table "goals", :force => true do |t|
     t.text     "six_week_goals"
@@ -184,23 +194,34 @@ ActiveRecord::Schema.define(:version => 20130721220840) do
   add_index "test_clients", ["project_id"], :name => "index_test_clients_on_project_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                                :default => "", :null => false
+    t.string   "encrypted_password",                   :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                        :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                                           :null => false
+    t.datetime "updated_at",                                           :null => false
     t.string   "name"
     t.string   "customer_id"
     t.string   "last_4_digits"
+    t.string   "invitation_token",       :limit => 60
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
