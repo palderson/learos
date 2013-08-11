b44e578fcae7a79c578bb4101d5751ea6ef77340
Mirko J <mirko114@gmail.com>
2013-08-11 13:40:19 +0200

    serialize :data, ActiveRecord::Coders::Hstore added to Overview model

--- app/models/overview.rb
+++ app/models/overview.rb
@@ -1,6 +1,7 @@
 class Overview < ActiveRecord::Base
   belongs_to :project
-  attr_accessible :elevator_pitch, :value_proposition, :internal_motivations, :file_url, :comments
+  attr_accessible :elevator_pitch, :value_proposition, :internal_motivations, :file_url, :data, :comments
+  serialize :data, ActiveRecord::Coders::Hstore
 
   acts_as_commentable
 end
