0c3cc24d72b3fdba2a7c672c4cf315a599efdf0c
Peter Alderson <peter.alderson@gmail.com>
2013-09-27 21:57:53 -0400

    added devise line

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,10 @@ Learos::Application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
-  :port           => '587',
+  :port           => '25',
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'learos.com',
+  :domain         => ENV['SENDGRID_DOMAIN'],
   :enable_starttls_auto => true
 }
