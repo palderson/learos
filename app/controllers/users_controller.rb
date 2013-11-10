81e6b1a4ee6a05326b2577107a3ad81d0d2378a9
Mirko J <mirko114@gmail.com>
2013-11-10 21:24:53 +0100

    bugfix, list collaboration projects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @collaborations = current_user.collaborations.map(&:project).select { |p| p.archived == false }
   end
   
   def update
