bfd021a6fda5797cf5ef7b6f45e4e68c2e44e025
Mirko J <mirko114@gmail.com>
2014-04-24 15:02:53 +0200

    seed.rb fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,20 @@
 # See http://railsapps.github.io/rails-environment-variables.html
 puts 'ROLES'
 YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+  # puts role.inspect
+  Role.where(name: role).first_or_create
+  # find_or_create_by depricated in rails 4
+  # Role.find_or_create_by_name({ :name => role }, :without_protection => true)
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+user = User.create :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
-user2 = User.find_or_create_by_email :name => 'Silver User', :email => 'peter.alderson@gmail.com', :password => 'changeme', :password_confirmation => 'changeme'
+user2 = User.create :name => 'Silver User', :email => 'peter.alderson@gmail.com', :password => 'changeme', :password_confirmation => 'changeme'
 user2.add_role :silver
-user3 = User.find_or_create_by_email :name => 'Gold User', :email => 'user3@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+user3 = User.create :name => 'Gold User', :email => 'user3@example.com', :password => 'changeme', :password_confirmation => 'changeme'
 user3.add_role :gold
-user4 = User.find_or_create_by_email :name => 'Platinum User', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+user4 = User.create :name => 'Platinum User', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
 user4.add_role :platinum
 puts "users: #{user2.name}, #{user3.name}, #{user4.name}"
